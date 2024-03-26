3288dbdbcf431fc46043cb54649f520faea716d6
akhilk2802 <akhilkumbar@outlook.com>
2024-03-25 20:35:14 -0400

    initialised springboot project

---
+++ FinalProject/src/test/java/com/csye6220/finalProject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csye6220.finalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
